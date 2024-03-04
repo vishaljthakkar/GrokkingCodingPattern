346cc37c9e4587c9be2527f71979f3dd90cd05bd
vishaljthakkar <vishaljthakkar@gmail.com>
2024-03-04 11:12:56 -0800

    [Sliding Window]: Longest Subarray with Ones after Replacement #1

---
+++ src/main/java/com/pattern/slidingwindow/hard/O8_ReplacingOnes.java
@@ -0,0 +1,10 @@
+package com.pattern.slidingwindow.hard;
+
+/*
+Given an array containing 0's and 1's, if you are allowed to replace no more than 'k' 0s  with 1s, find the length of the
+longest contiguous subarray having all 1s
+
+
+ */
+public class O8_ReplacingOnes {
+}
