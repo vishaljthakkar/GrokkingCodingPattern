769174bfbc940034a6b9a267c8ddd4f190b0b992
Vishal Thakkar <vishaljthakkar@gmail.com>
2024-03-07 12:50:50 -0800

    [NeetCode]: 03 Linked List  => 07 Doubly Linked List.  Algo DS Beginners

---
+++ src/main/java/com/neetcode/begineers/algods/o4linkedlist/o2double/DoublyLinkedListNode.java
@@ -0,0 +1,14 @@
+package com.neetcode.begineers.algods.o4linkedlist.o2double;
+
+public class DoublyLinkedListNode {
+
+    public int val;
+    public DoublyLinkedListNode next;
+    public DoublyLinkedListNode prev;
+
+    public DoublyLinkedListNode(int val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
